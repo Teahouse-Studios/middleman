97a82af3f956c60565a489fbfca132a58d463b9b
Dianliang233 <dianliang233@gmail.com>
2022-01-17 11:52:42 +0800

    Add legacy plugin

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vuetify from '@vuetify/vite-plugin'
+import legacy from '@vitejs/plugin-legacy'
 
 import path from 'path'
 
@@ -12,6 +13,9 @@ export default defineConfig({
     vuetify({
       autoImport: true,
     }),
+    legacy({
+      targets: ['defaults', 'not IE 11']
+    })
   ],
   define: { 'process.env': {} },
   resolve: {
