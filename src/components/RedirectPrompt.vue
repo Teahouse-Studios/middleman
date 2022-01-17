37691266924f2916c7faf901eadf86fd421db570
Dianliang233 <dianliang233@gmail.com>
2022-01-17 22:45:46 +0800

    Add progress circle while waiting to redirect

--- src/components/RedirectPrompt.vue
+++ src/components/RedirectPrompt.vue
@@ -1,13 +1,17 @@
 <template>
   <v-container class="mt-8">
     <v-row class="text-center" align="center" v-if="dest">
-      <v-col cols="12" md="8" class="mx-auto">
+      <v-col cols="12" md="8" class="mx-auto" v-if="!loading">
         <h1 class="mt-9 text-h4"><v-icon color="red-darken-2">mdi-alert</v-icon> 当前页面非茶馆工作室所属</h1>
         <p class="mt-1 text-body-2" style="overflow-x: auto;"><a :href="dest" target="_blank" rel="noopener noreferrer" class="text-grey-darken-1" style="text-overflow: ellipsis;">{{ dest }}</a></p>
         <p class="mt-3 text-body-1">您正在访问一个非茶馆工作室所属的页面。茶馆工作室不对其安全性、合法性做任何保证。请注意您的网络信息安全，请勿输入有关账号和密码。</p>
-        <v-btn class="mt-5 text-white" block flat color="indigo-darken-1" :href="dest">访问此页面</v-btn>
+        <v-btn class="mt-5 text-white" block flat color="indigo-darken-1" @click="go()">访问此页面</v-btn>
         <p class="bottom text-body-2 text-grey-darken-1">© {{ new Date().getFullYear() }} <a href="https://teahou.se/">Teahouse Studios</a> | 来源：{{ names[source] }}
         <br><a :href="terms[source]">服务条款</a> | <a :href="privacy[source]">隐私政策</a></p>
+      </v-col>
+      <v-col cols="12" md="8" class="mx-auto" v-if="loading">
+        <h1 class="mt-9 text-h4"><v-progress-circular indeterminate size="50" color="indigo" /> 正在重定向中</h1>
+        <p class="mt-1 text-body-2" style="overflow-x: auto;"><a :href="dest" target="_blank" rel="noopener noreferrer" class="text-grey-darken-1" style="text-overflow: ellipsis;">{{ dest }}</a></p>
       </v-col>
     </v-row>
     <p v-else>使用错误</p>
@@ -30,7 +34,9 @@ function endsWithAny(suffixes: string[], string: string) {
     }
     return false
 }
+let loading = $ref(false)
 if (endsWithAny(allowlist, new URL(dest!).hostname)) {
+  loading = true
   globalThis.location.href = dest!
 }
 
@@ -54,6 +60,11 @@ const privacy = {
 const terms = {
   akaribot: 'https://bot.teahou.se/-/215',
   default: 'https://teahou.se/terms',
+}
+
+function go() {
+  loading = true
+  globalThis.location.href = dest!
 }
 </script>
 
--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,9 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
+    vue({
+      reactivityTransform: true
+    }),
     // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
     vuetify({
       autoImport: true,
